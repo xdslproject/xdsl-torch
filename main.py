0f94eff7b29e0732a6a2e88ec2950e2cd4be420f
Sasha Lopoukhine <superlopuh@gmail.com>
2025-03-13 23:50:03 +0000

    add ci, tests, dependencies, and xdsl-torch-opt

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from xdsl-torch!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ xdsl_torch/tools/xdsl_torch_opt.py
@@ -0,0 +1,12 @@
+from xdsl.xdsl_opt_main import xDSLOptMain
+
+
+class TorchOptMain(xDSLOptMain): ...
+
+
+def main():
+    TorchOptMain().run()
+
+
+if __name__ == "__main__":
+    main()
