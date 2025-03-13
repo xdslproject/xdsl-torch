90b8addb92089a32170abe253e489bfc9e09cb9d
Max Manainen <maximmanainen@gmail.com>
2025-03-13 20:58:59 +0000

    simple export

--- example.py
+++ example.py
@@ -26,6 +26,7 @@ class AtenMulTensorOp(IRDLOperation):
     assembly_format = "$self `,` $other attr-dict `:` type($self) `,` type($other) `->` type($result)"
 
 xdsl_torch_ops: Dict[str, IRDLOperation] = {torch.ops.aten.mul.Tensor: AtenMulTensorOp}
+reverse_xdsl_torch_ops = {xdsl_op: torch_op for torch_op, xdsl_op in xdsl_torch_ops.items()}
 TorchDialect = Dialect("torch", [AtenMulTensorOp], [])
 
 
@@ -39,7 +40,6 @@ exported_program: torch.export.ExportedProgram = export(
     Mod(), args=example_args
 )
 
-print(exported_program.graph_signature)
 print(exported_program.graph)
 
 TORCH_DTYPE_TO_XDSL_TYPE = {
@@ -79,20 +79,45 @@ def import_program(prog: torch.export.ExportedProgram, func_name: str = "main") 
         xdsl_nodes : Dict[str, SSAValue] = {arg.name_hint: arg for arg in func_op.args}
 
         for node in prog.graph.nodes:
-                if node.op == "call_function":
-                    if node.target not in xdsl_torch_ops:
-                        raise NotImplementedError(
-                            f"FIX ME: Unimplemented call_function: target={node.target}, {node.meta}"
-                        )
-                    tensor_meta = node.meta["tensor_meta"]
-                    new_op = xdsl_torch_ops[node.target](
-                        operands=[xdsl_nodes[arg.name] for arg in node.args],
-                        result_types=[TensorType(TORCH_DTYPE_TO_XDSL_TYPE[tensor_meta.dtype], tensor_meta.shape)] # we currently think that everything returns a single tensor
+            if node.op == "call_function":
+                if node.target not in xdsl_torch_ops:
+                    raise NotImplementedError(
+                        f"FIX ME: Unimplemented call_function: target={node.target}, {node.meta}"
                     )
-                    new_op.result.name_hint = node.name
-                    xdsl_nodes[node.name] = new_op
+                tensor_meta = node.meta["tensor_meta"]
+                new_op = xdsl_torch_ops[node.target](
+                    operands=[xdsl_nodes[arg.name] for arg in node.args],
+                    result_types=[TensorType(TORCH_DTYPE_TO_XDSL_TYPE[tensor_meta.dtype], tensor_meta.shape)] # we currently think that everything returns a single tensor
+                )
+                new_op.result.name_hint = node.name
+                xdsl_nodes[node.name] = new_op
         func.ReturnOp(*[xdsl_nodes[out_node.arg.name] for out_node in sign.output_specs])
 
     return func_op
 
-print(import_program(exported_program))
+xdsl_op = import_program(exported_program)
+print(xdsl_op)
+
+def export_program(func_op: func.FuncOp) -> torch.fx.Graph:
+    # TODO: instead of a Graph object construct a full ExportedProgram
+    graph = torch.fx.Graph()
+    nodes: Dict[str, torch.fx.Node] = {}
+    for arg in func_op.args:
+        nodes[arg.name_hint] = graph.create_node("placeholder", arg.name_hint, None, None, arg.name_hint)
+    for op in func_op.body.ops:
+        if type(op) is func.ReturnOp:
+            graph.create_node("output", "", tuple(nodes[arg.name_hint] for arg in op.operands))
+        else:
+            nodes[op.results[0].name_hint] = graph.create_node(
+                "call_function",
+                reverse_xdsl_torch_ops[type(op)],
+                tuple(nodes[arg.name_hint] for arg in op.operands),
+                None,
+                op.results[0].name_hint
+            )
+    return graph
+
+graph = export_program(xdsl_op)
+print(graph)
+graph_mod = torch.fx.GraphModule(torch.nn.Module(), graph)
+print(torch.isclose(graph_mod.forward(example_args[0], example_args[1]), Mod().forward(example_args[0], example_args[1])).all().item())
