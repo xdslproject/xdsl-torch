ac600359aef882cc54c2a95e4b4022307a5ce0cb
Max Manainen <maximmanainen@gmail.com>
2025-03-20 00:14:34 +0000

    fix test definition

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -5,6 +5,8 @@ from xdsl_torch.dialects.torch_mapping import REVERSE_XDSL_TORCH_OPS, XDSL_TORCH
 from xdsl_torch.dialects.torch_ops import AtenMulTensorOp
 from xdsl_torch.utils.type_mapping import TORCH_DTYPE_TO_XDSL_TYPE
 
-assert XDSL_TORCH_OPS[torch.ops.aten.mul.Tensor] is AtenMulTensorOp  # type: ignore
-assert REVERSE_XDSL_TORCH_OPS[AtenMulTensorOp] is torch.ops.aten.mul.Tensor  # type: ignore
-assert TORCH_DTYPE_TO_XDSL_TYPE[torch.float64] is f64
+
+def test_mappings():
+    assert XDSL_TORCH_OPS[torch.ops.aten.mul.Tensor] is AtenMulTensorOp  # type: ignore
+    assert REVERSE_XDSL_TORCH_OPS[AtenMulTensorOp] is torch.ops.aten.mul.Tensor  # type: ignore
+    assert TORCH_DTYPE_TO_XDSL_TYPE[torch.float64] is f64
