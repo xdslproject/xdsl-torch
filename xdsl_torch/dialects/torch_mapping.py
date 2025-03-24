d2858177bc71524f986fca06adbfff9102d95b73
Max Manainen <maximmanainen@gmail.com>
2025-03-24 22:17:55 +0000

    init

--- xdsl_torch/dialects/torch_mapping.py
+++ xdsl_torch/dialects/torch_mapping.py
@@ -2,12 +2,12 @@ from typing import Any
 
 import torch
 
-from xdsl_torch.dialects.torch_ops import *
+from xdsl_torch.dialects.torch_dialect import *
 
 XDSL_TORCH_OPS: dict[Any, type] = {
-    torch.ops.aten.mul.Tensor: AtenMulTensorOp,
-    torch.ops.aten.sin.default: AtenSinOp,
-    torch.ops.aten.cos.default: AtenCosOp,
+    torch.ops.aten.mul.Tensor: Torch_AtenMulTensorOp,
+    torch.ops.aten.sin.default: Torch_AtenSinOp,
+    torch.ops.aten.cos.default: Torch_AtenCosOp,
 }
 
 REVERSE_XDSL_TORCH_OPS = {
