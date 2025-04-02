980be0c036a6ba4b83d032165c594f3749949ca9
Max Manainen <maximmanainen@gmail.com>
2025-04-02 17:19:48 +0100

    init

--- xdsl_torch/utils/import_program.py
+++ xdsl_torch/utils/import_program.py
@@ -1,11 +1,62 @@
+from itertools import zip_longest
+from typing import Any
+
 import torch
 from xdsl.builder import ImplicitBuilder
-from xdsl.dialects import func
-from xdsl.dialects.builtin import TensorType
+from xdsl.dialects import arith, func
+from xdsl.dialects.builtin import BoolAttr, FloatAttr, IntegerAttr, TensorType
 from xdsl.ir import SSAValue
 
 from xdsl_torch.dialects.torch_mapping import XDSL_TORCH_OPS
 from xdsl_torch.utils.type_mapping import TORCH_DTYPE_TO_XDSL_TYPE
+
+
+def literal_to_ssa(value: Any) -> tuple[str, SSAValue]:
+    match value:
+        case int():
+            attr = IntegerAttr.from_int_and_width(value, 32)
+        case float():
+            attr = FloatAttr(value, 32)
+        case bool():
+            attr = BoolAttr.from_bool(value)
+        case _:
+            raise NotImplementedError(
+                f"Default values not implemented for type {type(value)}"
+            )
+
+    new_const = arith.ConstantOp(attr, attr.get_type())
+    new_name = f"{type(value).__name__}{value}"
+    new_const.result.name_hint = new_name
+
+    return new_name, new_const.result
+
+
+def get_op_operands(
+    node: torch.fx.Node, xdsl_nodes: dict[str, SSAValue]
+) -> list[SSAValue]:
+    arguments: list[tuple[torch.Argument, Any]] = list(
+        zip_longest(node.target._schema.arguments, node.args)  # type: ignore
+    )
+    operands: list[SSAValue] = []
+
+    for arg_spec, arg_value in arguments:
+        if type(arg_value) is torch.fx.Node:
+            if arg_value.name not in xdsl_nodes:
+                raise Exception(
+                    f"Node {arg_value.name} should have been processed before"
+                )
+            operands.append(xdsl_nodes[arg_value.name])
+
+        if arg_value is None:
+            if not (
+                arg_spec.has_default_value() and arg_spec.default_value is not None
+            ):
+                raise Exception("A non provided argument must have a default value")
+            new_name, new_const = literal_to_ssa(arg_spec.default_value)
+            xdsl_nodes[new_name] = new_const
+            operands.append(new_const)
+
+    return operands
 
 
 def import_program(
@@ -27,7 +78,8 @@ def import_program(
     ):
         tensor_meta = all_producer_nodes[arg.arg.name].meta["tensor_meta"]
         return TensorType(
-            TORCH_DTYPE_TO_XDSL_TYPE[tensor_meta.dtype], tensor_meta.shape
+            TORCH_DTYPE_TO_XDSL_TYPE[tensor_meta.dtype],
+            tensor_meta.shape,  # type: ignore
         )
 
     inp_sign = list(map(make_tensor_type, prog.graph_signature.input_specs))
@@ -48,15 +100,12 @@ def import_program(
                     raise NotImplementedError(
                         f"Unimplemented: target={node.target}, {node.meta}"
                     )
-                arg_names = [
-                    arg.name if type(arg) is torch.fx.Node else "" for arg in node.args
-                ]
-                assert all(arg_names)
+                operands = get_op_operands(node, xdsl_nodes)
                 new_op = XDSL_TORCH_OPS[node.target](
-                    operands=[xdsl_nodes[name] for name in arg_names],
+                    operands=operands,
                     result_types=[
                         TensorType(
-                            TORCH_DTYPE_TO_XDSL_TYPE[node.meta["tensor_meta"].dtype],
+                            TORCH_DTYPE_TO_XDSL_TYPE[node.meta["tensor_meta"].dtype],  # type: ignore
                             node.meta["tensor_meta"].shape,
                         )
                     ],  # we currently think that everything returns a single tensor
--- xdsl_torch/utils/type_mapping.py
+++ xdsl_torch/utils/type_mapping.py
@@ -1,4 +1,11 @@
 import torch
-from xdsl.dialects.builtin import f16, f32, f64
+from xdsl.dialects.builtin import f16, f32, f64, i16, i32, i64
 
-TORCH_DTYPE_TO_XDSL_TYPE = {torch.float16: f16, torch.float32: f32, torch.float64: f64}
+TORCH_DTYPE_TO_XDSL_TYPE = {
+    torch.float16: f16,
+    torch.float32: f32,
+    torch.float64: f64,
+    torch.int16: i16,
+    torch.int32: i32,
+    torch.int64: i64,
+}
