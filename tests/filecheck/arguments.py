2ba7961613d3097cc9ca883e67877f4e4860ee3a
Max Manainen <maximmanainen@gmail.com>
2025-04-02 17:19:48 +0100

    init

---
+++ tests/filecheck/arguments.py
@@ -0,0 +1,22 @@
+# RUN: python %s | filecheck %s
+
+import torch
+from torch.export import export
+
+from xdsl_torch.utils.import_program import import_program
+
+
+class Diag(torch.nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.diagonal(x)
+
+
+exported_program: torch.export.ExportedProgram = export(
+    Diag(), args=(torch.randn(10, 10),)
+)
+
+# CHECK:        %int0 = arith.constant 0 : i32
+# CHECK-NEXT:   %int0_1 = arith.constant 0 : i32
+# CHECK-NEXT:   %int1 = arith.constant 1 : i32
+# CHECK-NEXT:   %diagonal = torch.aten.diagonal %x, %int0, %int0_1, %int1 : tensor<10x10xf32>, i32, i32, i32 -> tensor<10xf32>
+print(import_program(exported_program))
