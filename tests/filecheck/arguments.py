4c6cea2e923e558d2c4677bb8844f251915b20c5
Max Manainen <maximmanainen@gmail.com>
2025-04-02 22:48:51 +0100

    Constant arguments (#22)
    Now we can also pass constants as arguments to ops.

--- tests/filecheck/arguments.py
+++ tests/filecheck/arguments.py
@@ -20,3 +20,19 @@ exported_program: torch.export.ExportedProgram = export(
 # CHECK-NEXT:   %int1 = arith.constant 1 : i32
 # CHECK-NEXT:   %diagonal = torch.aten.diagonal %x, %int0, %int0_1, %int1 : tensor<10x10xf32>, i32, i32, i32 -> tensor<10xf32>
 print(import_program(exported_program))
+
+
+class Diag(torch.nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.diagonal(x, 2, 1, 0)
+
+
+# CHECK:        %int2 = arith.constant 2 : i32
+# CHECK-NEXT:   %int1 = arith.constant 1 : i32
+# CHECK-NEXT:   %int0 = arith.constant 0 : i32
+# CHECK-NEXT:   %diagonal = torch.aten.diagonal %x, %int2, %int1, %int0 : tensor<10x10xf32>, i32, i32, i32 -> tensor<8xf32>
+exported_program: torch.export.ExportedProgram = export(
+    Diag(), args=(torch.randn(10, 10),)
+)
+
+print(import_program(exported_program))
