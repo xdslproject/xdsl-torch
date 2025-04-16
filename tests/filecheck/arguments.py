374480294353c67421711bf2389e2f245301457e
Max Manainen <maximmanainen@gmail.com>
2025-04-16 02:57:48 +0200

    transfer tests to core ops

--- tests/filecheck/arguments.py
+++ tests/filecheck/arguments.py
@@ -38,19 +38,16 @@ exported_program: torch.export.ExportedProgram = export(
 print(import_program(exported_program))
 
 
-class Dist(torch.nn.Module):
-    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        return torch.nn.functional.pairwise_distance(x, y, 3, 1e-1, True)
+class Add(torch.nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x + 2.5
 
 
-exported_program: torch.export.ExportedProgram = export(
-    Dist(), args=(torch.randn(10), torch.randn(10))
-)
+exported_program: torch.export.ExportedProgram = export(Add(), args=(torch.randn(10),))
 
-# CHECK:        %float3.0 = arith.constant 3.000000e+00 : f32
-# CHECK-NEXT:   %float0.1 = arith.constant 1.000000e-01 : f32
-# CHECK-NEXT:   %boolTrue = arith.constant true
-# CHECK-NEXT:   %pairwise_distance = torch.aten.pairwise_distance %x, %y, %float3.0, %float0.1, %boolTrue : tensor<10xf32>, tensor<10xf32>, f32, f32, i1 -> tensor<1xf32>
+# CHECK:       %float2.5 = arith.constant 2.500000e+00 : f32
+# CHECK-NEXT:  %int1 = arith.constant 1 : i32
+# CHECK-NEXT:  %add = torch.aten.add.Tensor %x, %float2.5, %int1 : tensor<10xf32>, f32, i32 -> tensor<10xf32>
 print(import_program(exported_program))
 
 
